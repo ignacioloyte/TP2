8a685fdc58ba7d14016060a6068711613ade6612
Ignacio Loyte <iloyte@bancosanjuan.com>
2023-12-12 17:33:51 -0300

    bitacora

---
+++ CapaNegocio/CN_Log.cs
@@ -0,0 +1,33 @@
+﻿using CapaDatos;
+using CapaEntidad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaNegocio
+{
+
+    public class CN_Log
+    {
+        private LogData logData;
+
+
+        public void LogAction(string user, string action, string details)
+        {
+
+            logData = new LogData(Conexion.cadena);
+            LogEntry logEntry = new LogEntry
+            {
+                Timestamp = DateTime.Now,
+                User = user,
+                Action = action,
+                Details = details
+            };
+
+            logData.InsertLog(logEntry);
+        }
+    }
+}
+
